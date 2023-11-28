3ebae5afca94bde05df0875b842525b52da7a38a
Micael Santana <micaelsantana.dev@gmail.com>
2023-11-28 13:46:07 -0300

    added color background and change container dev photo

--- app/Components/header/NavBar.tsx
+++ app/Components/header/NavBar.tsx
@@ -1,36 +1,46 @@
-{/* Importações principais */}
+{
+  /* Importações principais */
+}
 import Image from "next/image";
 import Link from "next/link";
 
-{/* Estilos */}
+{
+  /* Estilos */
+}
 import styles from "./NavBar.module.css";
 
-{/* React-icons */}
+{
+  /* React-icons */
+}
 import { FaLinkedin, FaInstagramSquare, FaGithub } from "react-icons/fa";
 
 export default function NavBar() {
   return (
     <>
       <nav className={styles.NavBar}>
-        <div className={styles.ContentNavBar}>
-          <section className={styles.SectionPhoto}>
-            <figure>
-              <Image src={""} width={100} height={100} alt="Minha Foto"></Image>
-            </figure>
-          </section>
+        <section className={styles.SectionPhoto}>
+          <figure>
+            <Image
+              src={"/Images/micael.png"}
+              width={100}
+              height={100}
+              alt="Minha Foto"
+              className="DevPhoto"
+            ></Image>
+          </figure>
+        </section>
 
-          <section className={styles.SectionLinks}>
-            <Link href={"/"}>Projetos</Link>
-            <Link href={"/"}>Tecnologias</Link>
-            <Link href={"/"}>Sobre mim</Link>
-          </section>
+        <section className={styles.SectionLinks}>
+          <Link href={"/"}>Projetos</Link>
+          <Link href={"/"}>Tecnologias</Link>
+          <Link href={"/"}>Sobre mim</Link>
+        </section>
 
-          <section className={styles.SocialNetworks}>
-            <FaLinkedin />
-            <FaInstagramSquare />
-            <FaGithub />
-          </section>
-        </div>
+        <section className={styles.SocialNetworks}>
+          <FaLinkedin />
+          <FaInstagramSquare />
+          <FaGithub />
+        </section>
       </nav>
     </>
   );
