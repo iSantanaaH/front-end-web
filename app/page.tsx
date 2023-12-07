5654949eb437a140cd88006e7d3a37d39ae11d07
Micael Santana <micaelsantana.dev@gmail.com>
2023-12-07 19:15:24 -0300

    added section 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-{/* Componentes */}
+{
+  /* Componentes */
+}
 import NavBar from "./Components/header/NavBar";
+import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
     <main>
-      <NavBar />
+      <section>
+        <div>
+          <NavBar />
+        </div>
+      </section>
+
+      <section>
+        <div className={styles.Resume}>
+          <h2>Resumo</h2>
+          <p>
+            Ola, meu nome é <span>Micael Santana</span>, sou desenvolvedor
+            front-end, sou formado em Análise e Desenvolvimento de Sistemas na
+            UNOPOAR em Dezembro de 2023. Sou apaixonado por tecnologia e busco
+            sempre melhorar minhas habilidades na programação. Estou ancioso
+            para aplicar meus conhecimentos junto a equipe e contribuir para um
+            sucesso contínuo.
+          </p>
+          <button>Vamos navegar...</button>
+        </div>
+      </section>
     </main>
-  )
-};
+  );
+}
