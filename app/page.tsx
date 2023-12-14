7fc68ff656207cb953673c95cd6bd87813a4a29d
Micael Santana <micaelsantana.dev@gmail.com>
2023-12-14 11:41:42 -0300

    Change text resume and add new box to projects

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,14 @@ export default function HomePage() {
         <div className={styles.Resume}>
           <h2>Resumo</h2>
           <p>
-            Ola, meu nome é <span>Micael Santana</span>, sou desenvolvedor
-            front-end, sou formado em Análise e Desenvolvimento de Sistemas na
-            UNOPOAR em Dezembro de 2023. Sou apaixonado por tecnologia e busco
-            sempre melhorar minhas habilidades na programação. Estou ancioso
-            para aplicar meus conhecimentos junto a equipe e contribuir para um
-            sucesso contínuo.
+            Olá, meu nome é <span>Micael Santana</span>, sou um entusiasta e
+            desenvolvedor front-end, sou formado em Análise e Desenvolvimento de
+            Sistemas na UNOPOAR em Dezembro de 2023. Tenho uma paixão profunda
+            por tecnologia e estou em constante busca pelo aprimoramento das
+            minhas habilidades na programação. Com um background acadêmico sólido
+            e uma abordagem prática no desenvolvimento de soluções, estou
+            ansioso para aplicar meu conhecimento e experiência em um ambiente
+            colaborativo.
           </p>
           <button>
             <span>
@@ -36,7 +38,13 @@ export default function HomePage() {
 
         <div className={styles.BackgroundImage}>
           <figure>
-            <Image className={styles.LogoJavascript} src={"/Images/javascript.png"} width={500} height={300} alt="Logo react.js"/>
+            <Image
+              className={styles.LogoJavascript}
+              src={"/Images/javascript.png"}
+              width={500}
+              height={300}
+              alt="Logo react.js"
+            />
           </figure>
         </div>
       </section>
@@ -52,6 +60,78 @@ export default function HomePage() {
             </figure>
 
             <h3>Projeto 1</h3>
+
+            <p className={styles.Details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              eros magna, ornare non pulvinar eget, euismod ut nulla.
+            </p>
+          </div>
+          <div className={styles.Box}>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+
+            <h3>Projeto 2</h3>
+
+            <p className={styles.Details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              eros magna, ornare non pulvinar eget, euismod ut nulla.
+            </p>
+          </div>
+          <div className={styles.Box}>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+
+            <h3>Projeto 3</h3>
+
+            <p className={styles.Details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              eros magna, ornare non pulvinar eget, euismod ut nulla.
+            </p>
+          </div>
+          <div className={styles.Box}>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+
+            <h3>Projeto 4</h3>
+
+            <p className={styles.Details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              eros magna, ornare non pulvinar eget, euismod ut nulla.
+            </p>
+          </div>
+          <div className={styles.Box}>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+
+            <h3>Projeto 5</h3>
+
+            <p className={styles.Details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              eros magna, ornare non pulvinar eget, euismod ut nulla.
+            </p>
+          </div>
+          <div className={styles.Box}>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+
+            <h3>Projeto 6</h3>
+
+            <p className={styles.Details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              eros magna, ornare non pulvinar eget, euismod ut nulla.
+            </p>
+          </div>
+          <div className={styles.Box}>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+
+            <h3>Projeto 7</h3>
 
             <p className={styles.Details}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
