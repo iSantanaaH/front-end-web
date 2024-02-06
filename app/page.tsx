76ffd707a39d47cd06356518fb56550b7115e86d
Micael Santana <micaelsantana.dev@gmail.com>
2024-02-06 16:22:48 -0300

    added my image

--- app/page.tsx
+++ app/page.tsx
@@ -105,9 +105,9 @@ export default function HomePage() {
             <figure>
               <Image
                 className={styles.LogoImage}
-                src={"/Images/software.png"}
-                width={500}
-                height={500}
+                src={"/Images/minha_foto.png"}
+                width={700}
+                height={700}
                 alt="Logo react.js"
               />
             </figure>
